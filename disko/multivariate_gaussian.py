74d6ff38da7be83c48c991dcaccec5b1542d5c7e
tim <tim@elec.ac.nz>
2020-08-07 11:17:24 +1200

    Added multivariate transformations

--- disko/multivariate_gaussian.py
+++ disko/multivariate_gaussian.py
@@ -2,6 +2,16 @@ class MultivariateGaussian:
     '''
         Handle a multivariate gaussian with D dimensions, and real entries.
         
+        The basic idea is to take :
+            * measurements v_0, with covariance sigma_v
+            * model A, such that v = A x
+        
+        lh = MultivariateGaussian(v_0, sigma_v) % model for the measurement process, PDF for a measurmeent v
+        
+        prior = MultivariateGaussian(mu_x, sigma_x)   % distribution over vectors x
+        
+        posterior = bayes_update(lh, prior)
+        
     '''
     def __init__(self, D, mu, sigma):
         '''
@@ -10,17 +20,38 @@ class MultivariateGaussian:
         self.D = D
         self.mu = mu
         self.sigma = sigma
+        self.dtype = np.float64
         
         if (mu.shape[0] != D):
             raise ValueError('Mean mu {} must be a {}-vector'.format(mu.shape, D)
         if (sigma.shape[0] != (D,D)):
             raise ValueError('Covariance sigma {} must be a {}x{} square matrix'.format(sigma.shape, D)
-        
-    def bayes_update(self, lh, prior):
+    
+        self._sigma_inv = np.linalg.inv(self.sigma)
+    
+    def sigma_inv(self):
+        if self._sigma_inv is None:
+            self._sigma_inv = np.linalg.inv(self.sigma)
+        return self._sigma_inv
+
+    def bayes_update(self, lh, prior, measurements):
         '''
-            Return a new MultivariateGaussian, after update by likelihood.
+            Return a new MultivariateGaussian, after update by likelihood, and measurements
             
             @param lh, prior
+            
+            See section 3.1 of the documentation
         '''
-        pass
+        sigma_1 = np.linalg.inv(lh.sigma_inv() + prior.sigma_inv())
+        mu_1 = sigma_1 @ (prior.sigma_inv() @ self.mu + lh.sigma_inv() @ measurements)
     
+    
+    def linear_transform(self, A, b):
+        '''
+            Linear transform
+            y = A x + b
+        '''
+        sigma_1 = A @ self.sigma @ self.sigma.T
+        mu_1 = A @ self.mu + b
+        return MultivariateGaussian(mu_1, sigma_1)
+        
