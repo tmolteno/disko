880da3dd32bf2fb64211299859c924e04e120425
tim <tim@elec.ac.nz>
2020-09-08 14:32:34 +1200

    Added the start of a hexagon subsphere

--- disko/sphere.py
+++ disko/sphere.py
@@ -595,6 +595,48 @@ class HealpixSubSphere(HealpixSphere):
                 self.plot_x(plt, s.el_r, s.az_r)
 
 
+class HexagonGenerator(object):
+    '''
+        Returns a hexagon generator for hexagons of the specified size.
+        
+        https://variable-scope.com/posts/hexagon-tilings-with-python
+    '''
+    def __init__(self, edge_length):
+        self.edge_length = edge_length
+
+    @property
+    def col_width(self):
+        return self.edge_length * 3
+
+    @property
+    def row_height(self):
+        return math.sin(math.pi / 3) * self.edge_length
+
+    def __call__(self, row, col):
+        x = (col + 0.5 * (row % 2)) * self.col_width
+        y = row * self.row_height
+        for angle in range(0, 360, 60):
+            x += math.cos(math.radians(angle)) * self.edge_length
+            y += math.sin(math.radians(angle)) * self.edge_length
+        yield x
+        yield y
+
+
+
+class HexagonSubSphere(HealpixSphere):
+    
+    def __init__(self):
+        image = Image.new('RGB', (250, 250), 'white')
+        draw = Draw(image)
+        hexagon_generator = HexagonGenerator(40)
+        for row in range(7):
+            color = row * 10, row * 20, row * 30
+            for col in range(2):
+                hexagon = hexagon_generator(row, col)
+                draw.polygon(list(hexagon), Brush(color))
+        draw.flush()
+        image.show()
+
 if __name__=="__main__":
         sph = HealpixSphere(2)
         sph.to_svg(fname='sph.svg', pixels_only=True)
