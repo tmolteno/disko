953bfc962acdecc10aea4206117be8ebf9b63a45
tim <tim@elec.ac.nz>
2020-09-10 16:26:45 +1200

    Truncate the SVD to keep the cond(A) less than 50

--- disko/telescope_operator.py
+++ disko/telescope_operator.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler()) # Add other handlers if you're using this as a library
 logger.setLevel(logging.INFO)
 
-SVD_TOL=1e-6
+SVD_TOL=1e-3
 
 def plot_spectrum(s, n_s, n_v, rank, name):
     plt.figure(num=None, figsize=(6, 4), dpi=300, facecolor='w', edgecolor='k')
@@ -48,7 +48,7 @@ def tf_svd(x, tol=SVD_TOL):
     try:
         rank = np.min(np.argwhere(s < tol))
     except:
-        rank = x.shape[0]
+        rank = s.shape[0]
         
     return  [U, s, Vh], rank
 
@@ -57,8 +57,9 @@ def normal_svd(x, tol=SVD_TOL):
     n_s = x.shape[1]
 
     [U, s, Vh] = scipy.linalg.svd(np.array(x), full_matrices=True)
-    logger.info("s = {}".format(s[0:10]))
+    logger.info("Cond(A) = {}".format(s[0]/s[-1]))
 
+    tol = s[0]/50.0
     try:
         rank = np.min(np.argwhere(s < tol))
     except:
@@ -100,7 +101,7 @@ def dask_svd(x, tol=SVD_TOL):
         rank = np.min(np.argwhere(s < tol))
     except:
         logger.exception("Error in rank")
-        rank = n_v
+        rank = s.shape[0]
     logger.info("rank = {}".format(rank))
         
     # Now do QR decomposition to get the full U matrix (it is the u from svd with Q2 added)
