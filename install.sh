ef994be7a24e086c9ec4e3a0f20c94672cc0d7b8
tim <tim@elec.ac.nz>
2020-09-23 16:10:46 +1200

    Added little install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo apt install python3-pip \
+	python3-numpy python3-scipy python3-matplotlib \
+	python3-healpy python3-astropy python3-h5py \
+	python3-dask python3-distributed python3-sklearn 
