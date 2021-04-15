510e0e59e046693c372d2e65ece032029564a604
tim <tim@elec.ac.nz>
2021-04-15 16:22:28 +1200

    Better sequential testing

--- sequential_test.sh
+++ sequential_test.sh
@@ -3,9 +3,9 @@
 DIR=../tart2ms/
 FIRST=$(find $DIR -name 'test.ms_*' | sort | head -n 1)
 
-disko_bayes --ms $FIRST --mu --PNG --nside 20 --posterior post.h5
+disko_bayes --ms $FIRST --mu --PNG --nside 20 --posterior post.h5 --title 'seq' --dir seq_out
 
-for ms in $(find $DIR -name 'test.ms_*' | sort | head -n -1)
+for ms in $(find $DIR -name 'test.ms_*' | sort | tail -n +2)
 do
-    disko_bayes --ms $ms --prior post.h5 --mu --pcf --var --PNG --nside 20 --posterior post.h5 --title 'seq'
+    disko_bayes --ms $ms --prior post.h5 --mu --pcf --var --PNG --nside 20 --posterior post.h5 --title 'seq' --dir seq_out
 done
