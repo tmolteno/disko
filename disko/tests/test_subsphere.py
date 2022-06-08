e4d760e23e7b83c4858e57968941b8af83712d2e
tim <tim@elec.ac.nz>
2022-06-08 13:37:19 +1200

    Update to new sphere API

--- disko/tests/test_subsphere.py
+++ disko/tests/test_subsphere.py
@@ -19,30 +19,30 @@ class TestSubsphere(unittest.TestCase):
     def setUp(self):
         # Theta is co-latitude measured southward from the north pole
         # Phi is [0..2pi]
-        self.sphere = sphere.HealpixSubSphere.from_resolution(resolution=60.0, 
+        self.sphere = sphere.HealpixSubSphere.from_resolution(res_arcmin=60.0, 
                                               theta=np.radians(10.0), 
-                                              phi=0.0, radius=np.radians(1))
+                                              phi=0.0, radius_rad=np.radians(1))
 
     def test_big_subsphere(self):
         # Check that a full subsphere is the same as the sphere.
         res_deg = 3.0
-        big = sphere.HealpixSubSphere.from_resolution(resolution=res_deg*60.0, 
+        big = sphere.HealpixSubSphere.from_resolution(res_arcmin=res_deg*60.0, 
                                       theta=np.radians(0.0), phi=0.0, 
-                                      radius=np.radians(180))
-        old = sphere.HealpixSphere(20)
+                                      radius_rad=np.radians(180))
+        old = sphere.HealpixSphere(32)
 
-        self.assertEqual(big.nside, 20)
+        self.assertEqual(big.nside, 32)
         self.assertEqual(big.npix, old.npix)
 
     def test_tiny_subsphere(self):
         # Check that a full subsphere is the same as the sphere.
         res_deg = 0.5
-        big = sphere.HealpixSubSphere.from_resolution(resolution=res_deg*60.0, 
+        tiny = sphere.HealpixSubSphere.from_resolution(res_arcmin=res_deg*60.0, 
                                       theta=np.radians(0.0), 
-                                      phi=0.0, radius=np.radians(5))
+                                      phi=0.0, radius_rad=np.radians(5))
 
-        self.assertEqual(big.nside, 118)
-        self.assertEqual(big.npix, 312)
+        self.assertEqual(tiny.nside, 128)
+        self.assertEqual(tiny.npix, 364)
     
     def test_sizes(self):
        self.assertEqual(self.sphere.npix, self.sphere.el_r.shape[0])
@@ -51,9 +51,9 @@ class TestSubsphere(unittest.TestCase):
     def test_svg(self):
         res_deg = 10
         fname='test.svg'
-        big = sphere.HealpixSubSphere.from_resolution(resolution=res_deg*60.0, 
+        big = sphere.HealpixSubSphere.from_resolution(res_arcmin=res_deg*60.0, 
                                       theta=np.radians(0.0), phi=0.0, 
-                                      radius=np.radians(45))
+                                      radius_rad=np.radians(45))
 
         big.to_svg(fname=fname, pixels_only=True, show_cbar = False)
         self.assertTrue(os.path.isfile(fname))
@@ -62,9 +62,9 @@ class TestSubsphere(unittest.TestCase):
     def test_fits(self):
         res_deg = 10
         fname='test.fits'
-        big = sphere.HealpixSubSphere.from_resolution(resolution=res_deg*60.0, 
+        big = sphere.HealpixSubSphere.from_resolution(res_arcmin=res_deg*60.0, 
                                       theta=np.radians(0.0), phi=0.0, 
-                                      radius=np.radians(45))
+                                      radius_rad=np.radians(45))
 
         big.to_fits(fname=fname, fov=90)
         self.assertTrue(os.path.isfile(fname))
