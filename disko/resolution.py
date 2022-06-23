ca5550f1adc5713278e61bf8e8b01cd0977258d4
Tim Molteno <tim@4i.nz>
2022-06-24 11:05:55 +1200

    Update the Resolution class to display down to microarcsec

--- disko/resolution.py
+++ disko/resolution.py
@@ -4,6 +4,7 @@ class Resolution:
     
     def __init__(self, x_rad):
         self.x_rad = x_rad
+        self.x_deg = np.degrees(x_rad)
         
     @classmethod
     def from_deg(cls, x_deg):
@@ -21,11 +22,30 @@ class Resolution:
     def from_arcsec(cls, x_arcsec):
         return cls(np.radians(x_arcsec/3600))
     
+    def arcmin(self):
+        return self.x_deg * 60
+
+    def arcsec(self):
+        return self.x_deg * 3600
+
+    def mas(self):
+        return np.de
     def __repr__(self):
         d = np.degrees(self.x_rad)
         if d > 1:
             return f"{np.d:4.2f} deg"
-        if d > 1/60:
-            return f"{d*60:4.2f}\""
         
-        return f"{d*3600:4.2f}'"
+        if self.arcmin() > 1:
+            return f"{self.arcmin():4.2f}\""
+        
+        arcsec = self.arcsec()
+        if arcsec > 1:
+            return f"{arcsec:4.2f}'"
+        
+        mas = arcsec * 1000
+        if mas > 1:
+            return f"{mas:4.2f} mas"
+
+        uas = mas * 1000
+        if uas > 1:
+            return f"{uas:4.2f} uas"
